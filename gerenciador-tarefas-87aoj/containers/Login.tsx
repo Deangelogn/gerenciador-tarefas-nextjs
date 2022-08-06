cde15b0d97169d089f3450beb7a4196d75ff1f5a
Logon Aluno <logonrmlocal@fiap.com.br>
2022-08-06 10:45:29 -0300

    include styles

--- gerenciador-tarefas-87aoj/containers/Login.tsx
+++ gerenciador-tarefas-87aoj/containers/Login.tsx
@@ -1,19 +1,44 @@
-import React from "react";
+import React, {useState, ChangeEvent} from "react";
 
 export const Login = () => {
+
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const doLogin = (evento: ChangeEvent) => {
+        try {
+            evento.preventDefault();
+            if (!login || !password){
+                return setError('Favor informar usuário e senha');
+            }
+            setError('Dados validados com sucesso!')
+        }
+        catch(e){
+            console.log(e);
+        }
+    }
+
     return (
         <div className="container-login">
             <img className="logo" src="/logo.svg" alt="Logo Fiap"/>
             <form>
+                <p classname="error">{error}</p>
                 <div className="input">
                     <img src="/mail.svg" alt="Informe seu login"/>
-                    <input type="text" placeholder="login"/>
+                    <input type="text" placeholder="login" 
+                        value={login}
+                        onChange={evento => setLogin(evento.target.value)}
+                    />
                 </div>
                 <div className="input">
                     <img src="/lock.svg" alt="Informe sua senha"/>
-                    <input type="password" placeholder="senha"/>
+                    <input type="password" placeholder="senha"
+                        value={password}
+                        onChange={evento => setPassword(evento.target.value)}
+                    />
                 </div>
-                <button>Login</button>
+                <button onClick={doLogin}>Login</button>
             </form>
         </div>
     );
