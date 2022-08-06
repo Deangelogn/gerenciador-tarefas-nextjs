039a82883157541050b848594b07832dd34daad0
Logon Aluno <logonrmlocal@fiap.com.br>
2022-08-06 17:17:46 -0300

    list component

--- gerenciador-tarefas-87aoj/containers/Login.tsx
+++ gerenciador-tarefas-87aoj/containers/Login.tsx
@@ -1,64 +1,60 @@
-import React, {useState, MouseEvent} from "react";
-import { NextPage} from 'next';
-import { executeRequest } from "../services/apiServices";
+/* eslint-disable @next/next/no-img-element */
+import React, {useState, MouseEvent} from 'react';
+import Image from 'next/image'
+import { executeRequest } from '../services/apiServices';
+import { NextPage } from 'next';
+import { AccessTokenProps } from '../types/AccessTokenProps';
 
-type LoginProps = {
-    setAccessToken(e: string): void
-}
-
-export const Login:NextPage<LoginProps> = ({setAccessToken}) => {
+export const Login:NextPage<AccessTokenProps> = ({setAccessToken }) => {
 
     const [login, setLogin] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
 
-    const doLogin = async (evento: MouseEvent) => {
-        try {
+    const doLogin = async(evento : MouseEvent) => {
+        try{
             evento.preventDefault();
             setError('');
 
-            if (!login || !password){
-                return setError('Favor informar usuário e senha');
+            if(!login || !password){
+                return setError('Favor informar usuário e senha.');
             }
+
             const body = {login, password};
+
             const result = await executeRequest('login', 'POST', body);
-            console.log(result);
-
             if(!result || !result.data){
-                return setError('Ocorreu erro ao processar login, tente novamente!');
-            } 
+                return setError('Ocoreu erro ao processar login, tente novamente!');
+            }
 
-            const {name, email, token} =  result.data;
+            const {name, email, token} = result.data;
             localStorage.setItem('accessToken', token);
             localStorage.setItem('userName', name);
             localStorage.setItem('userMail', email);
-
             setAccessToken(token);
-
-        }catch(e: any){
+        }catch(e : any){
             console.log(e);
-            if (e?.response?.data?.error){
+            if(e?.response?.data?.error){
                 return setError(e?.response?.data?.error);
             }
-            setError('Ocorreu erro ao processar login, tente novamente!')
+            setError('Ocoreu erro ao processar login, tente novamente!');
         }
     }
 
     return (
         <div className="container-login">
-            <img className="logo" src="/logo.svg" alt="Logo Fiap"/>
+            <img src="/logo.svg" alt="Logo Fiap" className="logo"/>
             <form>
                 <p className="error">{error}</p>
                 <div className="input">
                     <img src="/mail.svg" alt="Informe seu login"/>
-                    <input type="text" placeholder="login" 
+                    <input type="text" placeholder="Login"
                         value={login}
-                        onChange={evento => setLogin(evento.target.value)}
-                    />
+                        onChange={evento => setLogin(evento.target.value)}/>
                 </div>
                 <div className="input">
                     <img src="/lock.svg" alt="Informe sua senha"/>
-                    <input type="password" placeholder="senha"
+                    <input type="password" placeholder="Senha"
                         value={password}
                         onChange={evento => setPassword(evento.target.value)}
                     />
@@ -66,5 +62,6 @@ export const Login:NextPage<LoginProps> = ({setAccessToken}) => {
                 <button onClick={doLogin}>Login</button>
             </form>
         </div>
+            
     );
 }
